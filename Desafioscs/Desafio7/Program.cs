e26df1679b622af3985acf8d735356bb9e36003c
enzzoh <enzzosamuel742@gmail.com>
2022-11-17 14:17:03 -0300

    Projetos c#

--- Desafioscs/Desafio7/Program.cs
+++ Desafioscs/Desafio7/Program.cs
@@ -6,7 +6,26 @@ namespace Desafio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 0;
+            double b = 0;
+            double[] valores = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Digite os valores");
+                valores[i] = Convert.ToDouble(Console.ReadLine());
+
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("");
+
+            a = valores[0] + valores[1] + valores[2] + valores[3];
+            b = (a / 4);
+
+
+            Console.WriteLine("a soma dos valores é {0} é a média e de {1}", a, b);
         }
     }
 }
---
+++ Desafioscs/Desafio8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Desafio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine(Math.Sqrt(x));
+        }
+    }
+}
